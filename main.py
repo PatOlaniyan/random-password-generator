9ff00e5dcc212211053feb3c4494716361ade68b
PatrickOlaniyan <Patrick_Olaniyan@yahoo.co.uk>
2025-04-26 18:15:19 +0100

    Added user input

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 
+# User input
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
